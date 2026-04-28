217dbb79a30a7b9df5be3cf52be77515fb010387
Ayush <ayushkumarnbd125@gmail.com>
2026-04-28 18:19:01 +0530

    restapi

--- src/database/db.js
+++ src/database/db.js
@@ -1,14 +1,11 @@
 import "dotenv/config";
-import {drizzle} from "drizzle-orm/postgres-js";
-import pg from "pg";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-
-if(!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is not defined in environment variables");
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
 }
 
- export const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+const client = postgres(process.env.DATABASE_URL);
 
-export const db = drizzle(pool);
+export const db = drizzle(client);
